c98396c19ae0d5d9f1f3da953118ced752cdd82e
dim131 <dim-los_13@hotmail.com>
2019-02-17 13:05:33 +0000

    Λύσεις για το πρόβλημα scidinner

---
+++ source_code/code/25-PDP/scidinner/scidinner_complicated_iterative.cc
@@ -0,0 +1,69 @@
+#include <stack>
+#include <stdio.h>
+#include <vector>
+
+const size_t MAXN = 1000000;
+
+std::vector<long> v[MAXN+1];
+
+std::vector<long> topo_sort;
+bool triggered[MAXN + 1];
+long max_path[MAXN + 1];
+
+void dfsIterative(long begin) {
+   std::stack<std::pair<long, bool>> s;
+   s.push(std::make_pair(begin, false));
+   while(!s.empty()) {
+      long u = s.top().first;
+      bool was_trigger_point = s.top().second;
+      s.pop();
+      if (was_trigger_point) {
+         topo_sort.push_back(u);
+         continue;
+      }
+      if (!triggered[u]) {
+         triggered[u] = true;
+         s.push(std::make_pair(u, true));
+         for (const auto& neigh : v[u]) {
+            if (!triggered[neigh]) {
+               s.push(std::make_pair(neigh, false));
+            }
+         }
+      }
+   }
+}
+
+int main() {
+   long N, M;
+   FILE *fi = fopen("scidinner.in", "r");
+   fscanf(fi, "%ld%ld", &N, &M);
+   for (long i = 0; i < M; ++i) {
+      long a, b;
+      fscanf(fi, "%ld%ld", &a, &b);
+      v[a].push_back(b);
+   }
+   fclose(fi);
+   
+   for (long i = 1; i <= N; ++i) {
+      if (!triggered[i]) {
+         dfsIterative(i);
+      }
+   }
+   
+   long max_max_path = 0;
+   for (long i = 0; i < topo_sort.size(); ++i) {
+      long u = topo_sort[i];
+      for (const auto& neigh : v[u]) {
+         max_path[u] = std::max(max_path[u], max_path[neigh] + 1);
+      }
+      max_max_path = std::max(max_max_path, max_path[u]);
+   }
+   
+   FILE *fo = fopen("scidinner.out", "w");
+   fprintf(fo, "%ld\n", max_max_path + 1);
+   fclose(fo);
+   
+   return 0;
+}
+
+   
---
+++ source_code/code/25-PDP/scidinner/scidinner_complicated_recursive.cc
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <vector>
+
+const size_t MAXN = 1000000;
+
+std::vector<long> v[MAXN+1];
+
+std::vector<long> topo_sort;
+bool visited[MAXN + 1];
+long max_path[MAXN + 1];
+
+void dfs(long u) {
+   for (const auto& neigh : v[u]) {
+      if (!visited[neigh]) {
+         visited[neigh] = true;
+         dfs(neigh);
+      }
+   }
+   topo_sort.push_back(u);
+}
+
+int main() {
+   long N, M;
+   FILE *fi = fopen("scidinner.in", "r");
+   fscanf(fi, "%ld%ld", &N, &M);
+   for (long i = 0; i < M; ++i) {
+      long a, b;
+      fscanf(fi, "%ld%ld", &a, &b);
+      v[a].push_back(b);
+   }
+   fclose(fi);
+   
+   for (long i = 1; i <= N; ++i) {
+      if (!visited[i]) {
+         visited[i] = true;
+         dfs(i);
+      }
+   }
+   
+   long max_max_path = 0;
+   for (long i = 0; i < topo_sort.size(); ++i) {
+      long u = topo_sort[i];
+      for (const auto& neigh : v[u]) {
+         max_path[u] = std::max(max_path[u], max_path[neigh] + 1);
+      }
+      max_max_path = std::max(max_max_path, max_path[u]);
+   }
+   
+   FILE *fo = fopen("scidinner.out", "w");
+   fprintf(fo, "%ld\n", max_max_path + 1);
+   fclose(fo);
+   
+   return 0;
+}
+
+   
