3a5de95eee46bedb38c252b5b2a495889ebd9843
dim131 <dim-los_13@hotmail.com>
2019-02-17 12:13:47 +0000

    Update generated_execution.sh

--- source_code/generated_execution.sh
+++ source_code/generated_execution.sh
@@ -27,6 +27,42 @@ echo "   SOLUTION : seti_official_pas"
 echo "   SOLUTION : seti_brute_force"
 array=(`seq 1 10`)
 run_test 'testdata/25-PDP/seti/seti.in#' 'testdata/25-PDP/seti/seti.out#' 'seti.in' 'seti.out' './code/25-PDP/seti/seti_brute_force.cc' 3 'g++' "${array[@]}" 
+echo "TASK : sound"
+echo "   SOLUTION : sound_official_cpp"
+array=(`seq 1 14`)
+run_test 'testdata/25-PDP/sound/sound.in#' 'testdata/25-PDP/sound/sound.out#' 'sound.in' 'sound.out' './code/25-PDP/sound/sound_official.cpp' 1 'g++' "${array[@]}" 
+echo "   SOLUTION : sound_efficient"
+array=(`seq 1 14`)
+run_test 'testdata/25-PDP/sound/sound.in#' 'testdata/25-PDP/sound/sound.out#' 'sound.in' 'sound.out' './code/25-PDP/sound/sound_efficient.cc' 1 'g++' "${array[@]}" 
+echo "   SOLUTION : sound_brute_force"
+array=( 1 2 3 4 7)
+run_test 'testdata/25-PDP/sound/sound.in#' 'testdata/25-PDP/sound/sound.out#' 'sound.in' 'sound.out' './code/25-PDP/sound/sound_brute_force.cc' 1 'g++' "${array[@]}" 
+echo "   SOLUTION : sound_semi_fast"
+array=(`seq 1 8`)
+run_test 'testdata/25-PDP/sound/sound.in#' 'testdata/25-PDP/sound/sound.out#' 'sound.in' 'sound.out' './code/25-PDP/sound/sound_semi_fast.cc' 1 'g++' "${array[@]}" 
+echo "TASK : telecom"
+echo "   SOLUTION : telecom_official_cpp"
+array=(`seq 1 31`)
+run_test 'testdata/25-PDP/telecom/telecom.in#' 'testdata/25-PDP/telecom/telecom.out#' 'telecom.in' 'telecom.out' './code/25-PDP/telecom/telecom_official.cpp' 1 'g++' "${array[@]}" 
+echo "   SOLUTION : telecom_efficient"
+array=(`seq 1 32`)
+run_test 'testdata/25-PDP/telecom/telecom.in#' 'testdata/25-PDP/telecom/telecom.out#' 'telecom.in' 'telecom.out' './code/25-PDP/telecom/telecom_efficient.cc' 1 'g++' "${array[@]}" 
+echo "   SOLUTION : telecom_n2"
+array=(`seq 1 21`)
+run_test 'testdata/25-PDP/telecom/telecom.in#' 'testdata/25-PDP/telecom/telecom.out#' 'telecom.in' 'telecom.out' './code/25-PDP/telecom/telecom_n2.cc' 1 'g++' "${array[@]}" 
+echo "   SOLUTION : telecom_n3"
+array=(`seq 1 13`)
+run_test 'testdata/25-PDP/telecom/telecom.in#' 'testdata/25-PDP/telecom/telecom.out#' 'telecom.in' 'telecom.out' './code/25-PDP/telecom/telecom_n3.cc' 1 'g++' "${array[@]}" 
+echo "TASK : triangle"
+echo "   SOLUTION : triangle_official_cpp"
+array=(`seq 1 16`)
+run_test 'testdata/25-PDP/triangle/triangle.in#' 'testdata/25-PDP/triangle/triangle.out#' 'triangle.in' 'triangle.out' './code/25-PDP/triangle/triangle_official.cpp' 1 'g++' "${array[@]}" 
+echo "   SOLUTION : triangle_efficient"
+array=(`seq 1 16`)
+run_test 'testdata/25-PDP/triangle/triangle.in#' 'testdata/25-PDP/triangle/triangle.out#' 'triangle.in' 'triangle.out' './code/25-PDP/triangle/triangle_efficient.cc' 1 'g++' "${array[@]}" 
+echo "   SOLUTION : triangle_brute_force"
+array=(`seq 1 4`)
+run_test 'testdata/25-PDP/triangle/triangle.in#' 'testdata/25-PDP/triangle/triangle.out#' 'triangle.in' 'triangle.out' './code/25-PDP/triangle/triangle_brute_force.cc' 1 'g++' "${array[@]}" 
 echo "TASK : colors"
 echo "   SOLUTION : colors_official"
 array=(`seq 1 31`)
