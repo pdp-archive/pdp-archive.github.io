0fd6f8eac846f0d59621fbd0fcadcf8656a49f07
kipoujr <kipoujr@hotmail.com>
2019-03-21 23:00:00 +0200

    Λύση και κώδικας για το πρόβλημα schoolnet (28-PDP, c)

---
+++ source_code/code/28-PDP/schoolnet/schoolnet.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAXN 1000001
+
+long weight[MAXN], vc_with[MAXN], vc_without[MAXN];
+vector<vector<long> > con(MAXN);
+
+long VC_with(long i, long par);
+
+long VC_without(long i, long par) {
+  if (vc_without[i] == -1 ) {
+    vc_without[i]=0;
+    for (long j=0;j<con[i].size();++j) {
+      long x = con[i][j];
+      if (x == par) continue;
+      vc_without[i] += VC_with(x,i);
+    }
+  }
+  return vc_without[i];
+}
+
+long VC_with (long i, long par) {
+  if (vc_with[i] == -1) {
+    vc_with[i] = weight[i];
+    for (long j=0;j<con[i].size();++j) {
+      long x = con[i][j];
+      if (x == par) continue;
+      vc_with[i] += min(VC_with(x,i),VC_without(x,i));
+    }
+  }
+  return vc_with[i];
+}
+
+int main () {
+  freopen("schoolnet.in","r",stdin);
+  freopen("schoolnet.out","w",stdout);
+  long N;
+  scanf("%ld",&N);
+  for (long i=1;i<=N;++i) scanf("%ld",&weight[i]);
+  for (long i=1;i<N;++i) {
+    long x,y;
+    scanf("%ld %ld",&x,&y);
+    con[x].push_back(y);
+    con[y].push_back(x);
+  }
+  memset(vc_with,-1,sizeof(vc_with));
+  memset(vc_without,-1,sizeof(vc_without));  
+  printf("%ld\n",min(VC_with(1,-1), VC_without(1,-1)));
+  return 0;
+}
