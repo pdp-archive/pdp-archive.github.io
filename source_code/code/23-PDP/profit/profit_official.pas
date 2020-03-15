7bda790ddd995a1b2ae6c4a2dbb5077c3b7a0ab1
dim131 <dim-los_13@hotmail.com>
2020-03-15 02:41:47 +0000

    Add official solution codes for profit

---
+++ source_code/code/23-PDP/profit/profit_official.pas
@@ -0,0 +1,34 @@
+(* Αλέξανδρος Μπιμπίκος *)
+
+program pdp2011;
+var
+  N,xi,i,min,x1:integer;
+  profit,cur_profit:real;
+  input_file,output_file:Text;
+begin
+  assign(input_file,'profit.in');
+  reset(input_file);
+  readln (input_file,N);
+  profit:=1;
+  read(input_file,x1);
+  min:=x1;
+  for i:=2 to N do
+    begin
+      read(input_file,xi);
+      if(xi<min) then
+        begin
+          min:=xi;
+        end
+      else
+        begin
+          cur_profit:=xi/min;
+          if (cur_profit>profit) then
+            profit:=cur_profit;
+        end;
+    end;
+  assign(output_file,'profit.out');
+  rewrite(output_file);
+  writeln (output_file,profit:8:3);
+  close(input_file);
+  close(output_file);
+end.
