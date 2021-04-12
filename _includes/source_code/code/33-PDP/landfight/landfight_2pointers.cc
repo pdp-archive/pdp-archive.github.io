29bd43d4938ca4ef64e0215168a80f8522e6dd60
switch <switch@fujitsu2.mrinfo>
2021-04-12 04:21:15 +0300

    33-pdp-c-1 landfights

---
+++ _includes/source_code/code/33-PDP/landfight/landfight_2pointers.cc
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <algorithm>
+
+const long MAXN = 1'000'000L;
+
+long A[MAXN];
+
+int main(){
+    freopen("landfight.in","r",stdin);
+    freopen("landfight.out","w",stdout);
+    
+    long N;
+    scanf("%ld",&N);
+    for(long i=1;i<=N;i++)
+        scanf("%ld",A+i);
+    
+    long ans=N,prefix=0,suffix=0,lptr=0,rptr=N+1;
+    while(lptr<rptr){
+        if(prefix == suffix){
+            ans = std::min(ans,rptr-lptr-1);
+            prefix += A[++lptr];
+            suffix += A[--rptr];
+        }
+        while(prefix < suffix && lptr<rptr)
+            prefix += A[++lptr];
+        while(suffix < prefix && lptr<rptr)
+            suffix += A[--rptr];
+    }
+
+    printf("%ld\n",ans);
+    return 0;
+}
---
+++ _includes/source_code/code/33-PDP/landfight/landfight_vector.cc
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+const long MAXN = 1'000'000L;
+
+long A[MAXN];
+struct myvector {
+    long head;
+    vector<long> pos;
+    myvector(){
+        head = -1;//δεν υπάρχουν στοιχεία στον πίνακα
+    }
+    long query(long i){
+        //αφαίρεσε τις θέσεις που ξεπεράστηκαν
+        while(head>=0 && pos[head]<=i)
+            head--;
+        return (head>=0)?pos[head]:(MAXN+1);
+    }
+    void push(long x){
+        pos.push_back(x);
+        head++;//δείκτης στο τελευταίο στοιχείο
+    }
+};
+
+unordered_map<int, myvector> M;
+
+int main(){
+    freopen("landfight.in","r",stdin);
+    freopen("landfight.out","w",stdout);
+    
+    long N;
+    scanf("%ld",&N);
+    for(long i=1;i<=N;i++)
+        scanf("%ld",A+i);
+    
+    M[0].push(N+1);//περίπτωση L=N,R=0
+    for(long suffix=0L,i=N;i>0;i--){
+        suffix +=A[i];
+        M[suffix].push(i);
+    }
+    
+    long ans = N;
+    for(long prefix=0L,i=1;i<=N;i++){
+        prefix += A[i];
+        if(M.find(prefix) == M.end())
+            continue;        
+        ans = min(ans,M[prefix].query(i)-i-1);
+    }
+    
+    printf("%ld\n",ans);
+    return 0;
+}
