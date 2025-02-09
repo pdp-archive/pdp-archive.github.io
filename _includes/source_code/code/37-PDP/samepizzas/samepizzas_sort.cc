31309e5f0d4fc98449ae4ceab6dd8776a2e41a1b
Dim131 <mail@dimitrioslos.com>
2025-02-09 04:54:48 +0100

    Add codes for samepizzas

---
+++ _includes/source_code/code/37-PDP/samepizzas/samepizzas_sort.cc
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+
+int main(){
+   FILE *in = fopen("samepizzas.in", "r");
+   FILE *out= fopen("samepizzas.out", "w");
+   long P,N,K;
+   fscanf(in, "%ld\n", &P);
+   fscanf(in, "%ld %ld\n", &N, &K);
+   
+   std::vector<long> A(N);
+   for(long i = 0; i < N; i++)fscanf(in, "%ld", &A[i]);
+   
+   // Το std::greater<long>() χρειάζεται ώστε η ταξινόμηση 
+   // να γίνει σε φθίνουσα σειρά.
+   std::sort(A.begin(), A.end(), std::greater<long>());
+    
+   fprintf(out, "%ld\n", A[K-1]);
+   fclose(out);
+   fclose(in);
+   return 0;
+}
---
+++ _includes/source_code/code/37-PDP/samepizzas/samepizzas_subtask3.cc
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+
+int main(){
+   FILE *in = fopen("samepizzas.in", "r");
+   FILE *out= fopen("samepizzas.out", "w");
+   long P,N,K;
+   fscanf(in, "%ld\n", &P);
+   fscanf(in, "%ld %ld\n", &N, &K);
+   
+   std::vector<long> A(N);
+   for(long i = 0; i < N; i++) {
+      fscanf(in, "%ld", &A[i]);
+   }
+   auto mx = *std::max_element(A.begin(), A.end());
+   fprintf(out, "%ld\n", mx);
+   
+   fclose(out);
+   fclose(in);
+   return 0;
+}
