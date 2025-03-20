4c9d044477db97b7ef341b9354f294160cb363ce
Makis Arsenis <marsenis@gmail.com>
2025-03-20 14:17:47 -0700

    Fix int types in optimal solution

--- _includes/source_code/code/37-PDP/tiphunting/optimal.cc
+++ _includes/source_code/code/37-PDP/tiphunting/optimal.cc
@@ -5,19 +5,19 @@
 // #define DEBUG
 
 #ifdef DEBUG
-#define debug(...) fprintf(stderr, __VA_ARGS__) 
+#define debug(...) fprintf(stderr, __VA_ARGS__)
 #else
 #define debug(...)
 #endif
 
 using namespace std;
 
-using ii = pair<int, int>;
-using iii = pair<int, pair<int, int>>;
-using vvii = vector<vector<ii>>;
-using vvi = vector<vector<int>>;
+using ll = pair<long, long>;
+using lll = tuple<long, long, long>;
+using vvll = vector<vector<ll>>;
+using vvl = vector<vector<long>>;
 
-long positive_part(long x) { return max(0L, x); }
+long long positive_part(long long x) { return max(0LL, x); }
 
 // Πρώτη διαπέραση η οποία υπολογίζει το `subtree_loop_opt` για την κορυφή `u`
 // κι όλους τους απογόνους της.
@@ -26,7 +26,7 @@ long positive_part(long x) { return max(0L, x); }
 // και καταλλήγει πάλι πίσω στο `u`, παραμένοντας στο υποδέντρο που ορίζει
 // η κορυφή `u` -- με άλλα λόγια, η διαδρομή απαγορεύεται να διασχίσει
 // τον δρόμο `(u, parent)`.
-void compute_subtree_loop_opt(vector<long>& subtree_loop_opt, const vvii& tree, const vector<int>& tip, int u, int parent) {
+void compute_subtree_loop_opt(vector<long long> &subtree_loop_opt, const vvll &tree, const vector<long> &tip, long u, long parent) {
   subtree_loop_opt[u] = tip[u];
 
   for (auto [v, w]: tree[u]) {
@@ -44,7 +44,7 @@ void compute_subtree_loop_opt(vector<long>& subtree_loop_opt, const vvii& tree, 
 // από την κορυφή `u`, καταλλήγει στη ρίζα τους δέντρου και
 // μένει πάντα ΕΚΤΟΣ του υποδέντρου που ορίζει η `u`. Το φιλοδώρημα της κορυφής
 // `u` ΔΕΝ προσμετράται.
-void compute_supertree_root_opt(vector<long>& supertree_root_opt, const vector<long>& subtree_loop_opt, const vvii& tree, int u, int parent, int w) {
+void compute_supertree_root_opt(vector<long long>& supertree_root_opt, const vector<long long>& subtree_loop_opt, const vvll& tree, long u, long parent, long w) {
   assert(0 <= u && u < tree.size());
   assert(-1 <= parent && parent < (long)tree.size());
 
@@ -65,7 +65,7 @@ void compute_supertree_root_opt(vector<long>& supertree_root_opt, const vector<l
 // supertree_loop_opt[u] = κέρδος της βέλτιστης διαδρομής η οποία ξεκινάει αλλά
 // ΚΑΙ καταλήγει στην κορυφή `u`, και μένει πάντα ΕΚΤΟΣ του υποδέντρου που
 // ορίζει η `u`. Το φιλοδώρημα της κορυφής `u` ΔΕΝ προσμετράται.
-void compute_supertree_loop_opt(vector<long>& supertree_loop_opt, const vector<long>& subtree_loop_opt, const vvii& tree, int u, int parent, int w) {
+void compute_supertree_loop_opt(vector<long long>& supertree_loop_opt, const vector<long long>& subtree_loop_opt, const vvll& tree, int u, int parent, int w) {
   assert(0 <= u && u < tree.size());
   assert(-1 <= parent && parent < (long)tree.size());
 
@@ -79,8 +79,8 @@ void compute_supertree_loop_opt(vector<long>& supertree_loop_opt, const vector<l
       compute_supertree_loop_opt(supertree_loop_opt, subtree_loop_opt, tree, v, u, w);
 }
 
-void preprocess(const vvii& tree, int u, vector<int>& depth, vector<int>& parent, vector<int>& parent_weight) {
-  const int n = tree.size();
+void preprocess(const vvll& tree, int u, vector<long>& depth, vector<long>& parent, vector<long>& parent_weight) {
+  const long n = tree.size();
   assert(depth.size() >= n);
   assert(parent.size() >= n);
   assert(parent_weight.size() >= n);
@@ -100,42 +100,42 @@ void preprocess(const vvii& tree, int u, vector<int>& depth, vector<int>& parent
 
 // Set `pred[h][u] = v` when `v` is the `2^h`-th predecessor of `u`
 // according to the `parent` relation.
-void compute_pred(const vector<int>& parent, vvi& pred) {
-  const int H = pred.size() - 1;
-  const int n = parent.size();
+void compute_pred(const vector<long> &parent, vvl &pred)
+{
+  const long H = pred.size() - 1;
+  const long n = parent.size();
 
-  for (int u = 0; u < n; ++u)
+  for (long u = 0; u < n; ++u)
     pred[0][u] = parent[u];
 
-  for (int h = 1; h <= H; ++h)
-    for (int u = 0; u < n; ++u) {
-      assert(0 <= pred[h-1][u] && pred[h-1][u] < n);
-      pred[h][u] = pred[h-1][ pred[h-1][u] ];
+  for (long h = 1; h <= H; ++h)
+    for (long u = 0; u < n; ++u)
+    {
+      assert(0 <= pred[h - 1][u] && pred[h - 1][u] < n);
+      pred[h][u] = pred[h - 1][pred[h - 1][u]];
     }
 }
 
-
-iii lca(const vector<vector<int>>& pred, const vector<int>& depth, int u, int v) {
-  const int H = pred.size() - 1;
+lll lca(const vector<vector<long>> &pred, const vector<long> &depth, long u, long v) {
+  const long H = pred.size() - 1;
 
   if (u == v)
-    return {u, {-1, -1}};
+    return {u, -1, -1};
 
   if (depth[u] < depth[v]) {
-    auto [w, ij] = lca(pred, depth, v, u);
-    auto [i, j] = ij;
-    return {w, {j, i}};
+    auto [w, i, j] = lca(pred, depth, v, u);
+    return {w, j, i};
   }
 
   if (depth[u] != depth[v]) {
-    for (int h = H; h >= 0; h--)
+    for (long h = H; h >= 0; h--)
       if (depth[ pred[h][u] ] > depth[v])
         u = pred[h][u];
     
     assert(depth[pred[0][u]] == depth[v]);
 
     if (pred[0][u] == v)
-      return { v, { u, -1} };
+      return { v,  u, -1 };
 
     u =pred[0][u];
   }
@@ -143,7 +143,7 @@ iii lca(const vector<vector<int>>& pred, const vector<int>& depth, int u, int v)
   assert(depth[u] == depth[v]);
   assert(u != v);
 
-  for (int h = H; h >= 0; --h) {
+  for (long h = H; h >= 0; --h) {
     if (pred[h][u] != pred[h][v]) {
       u = pred[h][u];
       v = pred[h][v];
@@ -151,27 +151,27 @@ iii lca(const vector<vector<int>>& pred, const vector<int>& depth, int u, int v)
   }
 
   assert(pred[0][u] == pred[0][v]);
-  return { pred[0][u], { u, v } };
+  return { pred[0][u], u, v };
 }
 
 int main() {
   int subtask;
   scanf("%i", &subtask);
 
-  int n, q;
-  scanf("%i%i", &n, &q);
+  long n, q;
+  scanf("%li%li", &n, &q);
   
-  vector<int> tip(n);
-  for (int i = 0; i < n; ++i)
-    scanf("%i", &tip[i]);
+  vector<long> tip(n);
+  for (long i = 0; i < n; ++i)
+    scanf("%li", &tip[i]);
 
   // Αναπαράσταση του δέντρου με adjacency list:
   // To `tree[u]` περιέχει ένα vector με pairs `(v, w)` για κάθε κορυφή `v` που
   // συνδέεται με τη `u` με κόστός `w`.
-  vvii tree(n, vector<ii>{});
-  for (int i = 0; i < n-1; ++i) {
-    int u, v, w;
-    scanf("%i%i%i", &u, &v, &w);
+  vvll tree(n);
+  for (long i = 0; i < n-1; ++i) {
+    long u, v, w;
+    scanf("%li%li%li", &u, &v, &w);
     assert(1 <= u && u <= n);
     assert(1 <= v && v <= n);
 
@@ -182,24 +182,24 @@ int main() {
   debug("Read the tree\n");
 
   // "Hang" the tree from node 0 and compute the depth of each node.
-  vector<int> depth(n, 0), parent(n, 0), parent_weight(n, 0);
+  vector<long> depth(n, 0), parent(n, 0), parent_weight(n, 0);
   debug("Created vectors\n");
   preprocess(tree, 0, depth, parent, parent_weight);
   debug("preprocessed\n");
 
-  int max_depth = 0;
-  for (int i = 0; i < n; ++i)
+  long max_depth = 0;
+  for (long i = 0; i < n; ++i)
     max_depth = max(max_depth, depth[i]);
 
   // Using the parent relation, compute `pred[h][u]`, the `2^h`-th predecessor
   // of each node `u` for every `h \in {0, 1, ..., ceil(log_2(n-1))}`.
   // const int H = int(ceil(log2(n-1)));
-  const int H = int(ceil(log2(max_depth)));
-  vector<vector<int>> pred(H+1, vector<int>(n, 0));
+  const long H = long(ceil(log2(max_depth)));
+  vector<vector<long>> pred(H+1, vector<long>(n, 0));
   compute_pred(parent, pred);
   debug("computed pred\n");
 
-  vector<long> subtree_loop_opt(n), supertree_root_opt(n), supertree_loop_opt(n);
+  vector<long long> subtree_loop_opt(n), supertree_root_opt(n), supertree_loop_opt(n);
   compute_subtree_loop_opt(subtree_loop_opt, tree, tip, 0, -1);
   debug("done with pass1\n");
   compute_supertree_root_opt(supertree_root_opt, subtree_loop_opt, tree, 0, -1, -1);
@@ -207,23 +207,22 @@ int main() {
   compute_supertree_loop_opt(supertree_loop_opt, subtree_loop_opt, tree, 0, -1, -1);
   debug("done with pass3\n");
 
-  for (int i = 0; i < q; ++i) {
-    int src, dst;
-    scanf("%i%i", &src, &dst);
+  for (long i = 0; i < q; ++i) {
+    long src, dst;
+    scanf("%li%li", &src, &dst);
     src -= 1;
     dst -= 1;
 
     if (src == dst) {
-      printf("%li\n", subtree_loop_opt[src] + supertree_loop_opt[src]);
+      printf("%lli\n", subtree_loop_opt[src] + supertree_loop_opt[src]);
       continue;
     }
 
-    auto [mid, uv] = lca(pred, depth, src, dst);
-    auto [u, v] = uv;
+    auto [mid, u, v] = lca(pred, depth, src, dst);
     assert(u != -1 || v != -1);
-    debug("LCA(%i, %i) = (%i, (%i, %i))\n", src+1, dst+1, mid+1, u+1, v+1);
+    debug("LCA(%li, %li) = (%li, %li, %li)\n", src+1, dst+1, mid+1, u+1, v+1);
 
-    long sol = 0;
+    long long sol = 0;
     if (u == -1) {
       // src is an ancestor of dst.
       assert(mid == src);
@@ -238,25 +237,25 @@ int main() {
       assert(pred[0][v] == mid);
 
       sol = supertree_root_opt[src] + subtree_loop_opt[src] - supertree_root_opt[u];
-      debug("%li, ", sol);
+      debug("%lli, ", sol);
       sol += supertree_root_opt[dst] + subtree_loop_opt[dst] - supertree_root_opt[v];
-      debug("%li, ", sol);
+      debug("%lli, ", sol);
       sol -= (parent_weight[u] + parent_weight[v]);
       debug("%li, ", sol);
 
       sol += subtree_loop_opt[mid];
-      debug("%li, ", sol);
+      debug("%lli, ", sol);
       sol -= positive_part(subtree_loop_opt[u] - 2*parent_weight[u]);
-      debug("%li, ", sol);
+      debug("%lli, ", sol);
       sol -= positive_part(subtree_loop_opt[v] - 2*parent_weight[v]);
-      debug("%li, ", sol);
+      debug("%lli, ", sol);
 
       sol += supertree_loop_opt[mid];
-      debug("%li, ", sol);
+      debug("%lli, ", sol);
       debug("\n");
     }
 
-    printf("%li\n", sol);
+    printf("%lli\n", sol);
   }
 
   return 0;
