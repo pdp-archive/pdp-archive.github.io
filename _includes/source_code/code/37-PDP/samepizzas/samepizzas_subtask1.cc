b6922185705e6f5c8abe764e983bb19dadba898e
Dimitris Los <dimitrislos13@gmail.com>
2025-03-22 15:34:53 +0000

    36-samepizzas subtask1

--- _includes/source_code/code/37-PDP/samepizzas/samepizzas_subtask1.cc
+++ _includes/source_code/code/37-PDP/samepizzas/samepizzas_subtask1.cc
@@ -7,13 +7,13 @@ int main(){
    long P, N, K;
    fscanf(in, "%ld", &P);
    fscanf(in, "%ld%ld", &N, &K);
-   fclose(in);
    
    std::vector<long> A(N);
    for(long i = 0; i < N; i++) {
       fscanf(in, "%ld", &A[i]);
    }
-    
+   fclose(in);
+   
    FILE *out= fopen("samepizzas.out", "w");
    if (K == 1) fprintf(out, "%ld\n", std::max(A[0], A[1]));
    else fprintf(out, "%ld\n", std::min(A[0], A[1]));
