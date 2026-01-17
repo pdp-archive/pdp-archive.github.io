4fbadafe1aab3346a02b849f372ad86357af9aca
Dimitris Los <dimitrislos13@gmail.com>
2026-01-17 01:31:14 +0000

    Replies to comments for 38-a-ovens

--- _includes/source_code/code/38-PDP/ovens/ovens_efficient.cc
+++ _includes/source_code/code/38-PDP/ovens/ovens_efficient.cc
@@ -14,16 +14,18 @@ int main(){
   scanf("%ld", &N);
    
   for(long i = 0; i < N; ++i) {
-    scanf("%ld", &a[i]);
+    int temp;
+    scanf("%d", &temp);
+    a[i] = temp;
   }
   for(long i = 0; i < N; ++i) {
     scanf("%ld", &k[i]);
   }
    
   long num_switch = 0; // Το πλήθος των διακοπτών που πατήσαμε.
-  bool toggle = 0; // Το πλήθος των ενεργών διακοπτών mod 2.
+  bool active_switch_mod_2 = 0; // Το πλήθος των ενεργών διακοπτών mod 2.
   for (long i = 0; i < N; ++i) {
-    bool is_current_oven_on = a[i] ^ toggle;
+    bool is_current_oven_on = a[i] ^ active_switch_mod_2;
     if (is_current_oven_on) { 
       // Πατάμε τον i-οστό διακόπτη.
       ++num_switch;
@@ -32,12 +34,12 @@ int main(){
       switch_ending[i + k[i]] = !switch_ending[i + k[i]];
       // Το πλήθος των ενεργών διακοπτών αλλάζει κατά ένα
       // (άρα το υπόλοιπο με το δύο αντιστρέφεται).
-      toggle = !toggle;
+      active_switch_mod_2 = !active_switch_mod_2;
     }
     // Αν υπάρχει μονό πλήθος διακοπτών που τελειώνει στο i, 
     // τότε αλλάζουμε το υπόλοιπο των ενεργών διακοπτών.
     if (switch_ending[i]) {
-      toggle = !toggle;
+      active_switch_mod_2 = !active_switch_mod_2;
     }
   }
 
--- _includes/source_code/code/38-PDP/ovens/ovens_sub_4.cc
+++ _includes/source_code/code/38-PDP/ovens/ovens_sub_4.cc
@@ -9,7 +9,9 @@ int main(){
   scanf("%ld", &N);
    
   for(long i = 0; i < N; ++i) {
-    scanf("%ld", &a[i]);
+    int temp;
+    scanf("%d", &temp);
+    a[i] = temp;
   }
   for(long i = 0; i < N; ++i) {
     long ignore;
@@ -17,11 +19,11 @@ int main(){
   }
   
   long num_switch = 0;
-  bool toggle = 0;
+  bool active_switch_mod_2 = 0;
   for (long i = 0; i < N; ++i) {
-    if (a[i] ^ toggle) {
+    if (a[i] ^ active_switch_mod_2) {
       ++num_switch;
-      toggle = !toggle;
+      active_switch_mod_2 = !active_switch_mod_2;
     }
   }
     
